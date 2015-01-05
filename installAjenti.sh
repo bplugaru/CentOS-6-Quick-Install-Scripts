13fbd3ecbb5a0f6d9f862887790ee277ef4f26c5
bogdan plugaru <bplugaru@gmail.com>
2015-01-05 19:29:19 +0200

    update curl params

--- installAjenti.sh
+++ installAjenti.sh
@@ -4,12 +4,17 @@
 # source <(curl https://raw.githubusercontent.com/Eugeny/ajenti/master/scripts/install-rhel7.sh | sh --insecure)
 
 
-source <(curl https://raw.githubusercontent.com/Eugeny/ajenti/master/scripts/install-rhel7.sh | sh --insecure)
+source <(curl --insecure https://raw.githubusercontent.com/Eugeny/ajenti/master/scripts/install-rhel7.sh | sh)
 
-yum  install ajenti-v ajenti-v-nginx ajenti-v-nodejs 
+yum  install ajenti-v status-v-nginx ajenti-v-nodejs 
+yum install firewalld -y
 
 service ajenti restart
 
+
+
+systemctl start firewalld
+systemctl status firewalld -l
 
 firewall-cmd --permanent --zone=public --add-port=8000/tcp
 firewall-cmd --reload
--- installEnv.sh
+++ installEnv.sh
@@ -9,13 +9,13 @@ yum upgrade -y
 
 
 echo "[-]  Install NodeJS"
-source <(curl -s https://github.com/bplugaru/CentOS-7-Quick-Install-Scripts/blob/master/installNodeJS.sh --insecure)
+source <(curl -s --insecure https://github.com/bplugaru/CentOS-7-Quick-Install-Scripts/blob/master/installNodeJS.sh)
 
  
 echo "[-]  Install MongoDB"
-source <(curl -s https://github.com/bplugaru/CentOS-7-Quick-Install-Scripts/blob/master/installMongoDB.sh --insecure)
+source <(curl -s --insecure https://github.com/bplugaru/CentOS-7-Quick-Install-Scripts/blob/master/installMongoDB.sh)
 
 
 echo "[-]  Install Ajenti"
-source <(curl -s https://raw.githubusercontent.com/bplugaru/CentOS-7-Quick-Install-Scripts/master/installAjenti.sh --insecure)
+source <(curl -s --insecure https://raw.githubusercontent.com/bplugaru/CentOS-7-Quick-Install-Scripts/master/installAjenti.sh)
 
